8fb80205f3362ffc554965a4068bea25fb62237b
Alan Hill <alan.hill@shopify.com>
2016-09-26 18:11:28 -0400

    rubo

--- app/models/buzzer.rb
+++ app/models/buzzer.rb
@@ -1,16 +1,14 @@
 class Buzzer
   # put your own credentials here
 
-# this costs money btw
-##########
-##########
-#  @client.messages.create(
-#    from: '+16136995500',
-#    to: '+12269270238',
-#    body: 'Yo how dope is this dawg'
-#  )
-##########
-##########
-
-
+  # this costs money btw
+  ##########
+  ##########
+  #  @client.messages.create(
+  #    from: '+16136995500',
+  #    to: '+12269270238',
+  #    body: 'Yo how dope is this dawg'
+  #  )
+  ##########
+  ##########
 end

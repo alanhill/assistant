efd88da9fd766b357ee7c78b0c7261f53d7a4471
Alan Hill <alan.hill@shopify.com>
2016-10-19 11:31:40 -0400

    ..

--- app/models/concerns/weather.rb
+++ app/models/concerns/weather.rb
@@ -21,7 +21,7 @@ module Weather
   end
 
   def translate_temperature_response(temperature)
-    "Them temp is #{temperature['temp']} degrees celcius."
+    "The temp is #{temperature['temp']} degrees celcius."
   end
 
   def translate_sunrise_sunset_response(times_of_day)

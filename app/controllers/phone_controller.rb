41ab47fed3381375201a96dcc60c7a11f24bdb6f
Alan Hill <alan.hill@shopify.com>
2016-10-20 18:56:21 -0400

    test till tone figured out

--- app/controllers/phone_controller.rb
+++ app/controllers/phone_controller.rb
@@ -11,14 +11,14 @@ class PhoneController < ApplicationController
   def buzz_handler
     redirect_to '/call' unless params['Digits'].present?
 
-    if params['Digits'] == '1313'
+    if params['Digits'] == '1'
       response = Twilio::TwiML::Response.new do |r|
-        r.Say 'Buzzing you in!'
+        r.Say 'Testing..'
         r.Dial '9'
       end
-    elsif params['Digits'] == '0000'
+    elsif params['Digits'] == '0'
       response = Twilio::TwiML::Response.new do |r|
-        r.Say 'Okay, dialing Alan. One moment.'
+        r.Say 'One moment..'
         r.Dial ENV['MY_NUMBER']
       end
     end
@@ -29,10 +29,7 @@ class PhoneController < ApplicationController
   def buzz_answerer
     response = Twilio::TwiML::Response.new do |r|
       r.Say "Hello!"
-      r.Gather numDigits: '4', action: '/phone/buzz_handler', method: 'post' do |g|
-        g.Say 'Enter the code if you have it'
-        g.Say 'Or press 0000 to reach me if you donn\'t'
-      end
+      r.Gather numDigits: '1', action: '/phone/buzz_handler', method: 'post'
   end
 
   render_twiml response

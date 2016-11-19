1f753200fcc0af994c286f2d11771958b30bebb5
Alan Hill <alan.hill@shopify.com>
2016-11-19 14:44:02 -0500

    Add User, sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def create
+    email = auth_hash['info']['email']
+    name = auth_hash['info']['name']
+    @user = User.create_with(name: name).find_or_create_by(email: email)
+    session[:user_id] = @user.id
+    flash[:notice] = "Account for #{name} successfully created."
+    redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+end
